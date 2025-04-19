aa5f36519df0988c7c5f778990fdfe2b7d17805c
Buoyairican <154003630+Buoyairican@users.noreply.github.com>
2025-04-19 14:41:52 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 model = joblib.load('spam_model.pkl')
 vectorizer = joblib.load('vectorizer.pkl')  # optional if you're using text input
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Message(BaseModel):
     text: str
